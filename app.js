9053c5d960d2b72b1e773e97261a61d39f0acb0a
eekulev-coder <eekulev@gmail.com>
2026-08-15 22:26:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -538,7 +538,7 @@ document.querySelectorAll('.card-thumb[data-yt]').forEach(function(img){
       iframe.setAttribute('loading','lazy');
       parent.appendChild(iframe);
       parent._iframe=iframe;
-    },0.900);
+    },0.9000);
   });
   parent.addEventListener('mouseleave',function(){
     clearTimeout(timer);
